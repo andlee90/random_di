7f4e000f16e2be411c85460b6ea3f382f49ec0d4
Andrew Smith <andrewsmith@Andrew-Smiths-MacBook.local>
2015-11-19 17:37:35 -0500

    Version 1.1
    * Fix di string quotes
    * Fix prompt typos
    * Add top and bottom to di
    * Add comments

--- random_di.py
+++ random_di.py
@@ -1,18 +1,17 @@
-#TODO:
-# a. fix roll output quotes
-# b. more robust play-again loop
-# c. intro menu select
-
 # script to determine the outcome(s) of any number of dice rolls
+# by Andrew Smith (2015)
+
 from random import randint
 
-#--------------------------------------------------------------------------------
+#---------dice_roller function to determine roll outcomes----------------------------
 def dice_roller(dice, rolls, sides):
 
 	show_di = '|   |'
 	show_di_1 = '| * |'
 	show_di_2 = '|* *|'
 	show_di_3 = '|***|'
+	show_di_t = ' ___ '
+	show_di_b = ' --- '
 
 	roll_counter = 0
 	di_counter = 0
@@ -26,35 +25,47 @@ def dice_roller(dice, rolls, sides):
 			di_counter+=1
 			num_side = randint(1, sides)
 			print "Roll %r, Di %r: %r" % (roll_counter, di_counter, num_side)
-			if num_side <= 6:
+			if num_side <= 6: # print di symbol if di is six-sided
 				if num_side == 1:
-					print '%r' % show_di_1
-					print '%r' % show_di
+					print show_di_t
+					print show_di_1
+					print show_di
+					print show_di_b
 				elif num_side == 2:
-					print '%r' % show_di_2
-					print '%r' % show_di
+					print show_di_t
+					print show_di_1
+					print show_di_1
+					print show_di_b
 				elif num_side == 3:
-					print '%r' % show_di_2
-					print '%r' % show_di_1
+					print show_di_t
+					print show_di_1
+					print show_di_2
+					print show_di_b
 				elif num_side == 4:
-					print '%r' % show_di_2
-					print '%r' % show_di_2
+					print show_di_t
+					print show_di_2
+					print show_di_2
+					print show_di_b
 				elif num_side == 5:
-					print '%r' % show_di_3
-					print '%r' % show_di_2
+					print show_di_t
+					print show_di_2
+					print show_di_3
+					print show_di_b
 				elif num_side == 6:
-					print '%r' % show_di_3.strip('"\'')
-					print '%r' % show_di_3.strip('"\'')
+					print show_di_t
+					print show_di_3
+					print show_di_3
+					print show_di_b
 	print " "
 #-----------------------------------------------------------------------------------
-
-
-menu = """
+title = """
  __________________
-| Dice Roller v1.0 |
+| Dice Roller v1.1 |
 | by Andrew Smith  |
 |__________________|
+"""
 
+menu = """
 Enter the number of dice to roll, the number
 of rolls, and the number of sides per di in the 
 following format: #dice #rolls #sides
@@ -62,28 +73,29 @@ following format: #dice #rolls #sides
 --- or ---
 
 Select from the following presets:
+
 a. 3 man attack (Risk)
-b. 2 man atack (Risk)
-c. 1 man attack (Risk)
+b. 2 man attack or defend (Risk)
+c. 1 man attack or defend (Risk)
 
 --- or ---
 
-Type menu to repeat this menu
+Type 'menu' to repeat this menu
 
 --- or ---
 
 Type 'quit' to quit
 		"""
-
+print title
 print menu
 
 again = 0
 
-while again != 1:
+while again != 1: # prompts the user until 'quit' is entered
 
 	roll_choice = raw_input("> ")
 
-	if roll_choice.isalpha():
+	if roll_choice.isalpha(): # if input starts with a letter
 		if roll_choice == 'a':
 			dice_roller(3, 1, 6)
 		elif roll_choice == 'b':
@@ -97,7 +109,7 @@ while again != 1:
 		else:
 			print "Invalid choice.\n"
 
-	elif roll_choice[0].isdigit():
+	elif roll_choice[0].isdigit(): # if input starts with a number
 		choices = roll_choice.split()
 		size = len(choices)
 		if size == 3:
